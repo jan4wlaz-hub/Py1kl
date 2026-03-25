5d2106f36c84d3abe8468a5df5d01fc5aa2874bb
jan4wlaz-hube <jan4wlaz@gmail.com>
2026-03-25 12:06:08 +0100

    gfds

--- iteracja i rekurencja.py
+++ iteracja i rekurencja.py
@@ -49,6 +49,17 @@ def silnia(n):
     for i in range(1, n + 1):
         wynik *= i
     return wynik
+n=input("Podaj liczbę: ")
+print(silnia(n))
+
+def silnia_rekurencyjna(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * silnia_rekurencyjna(n - 1)
+n=input("Podaj liczbę: ")
+print(silnia_rekurencyjna(n))
+
 #2
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
@@ -60,3 +71,52 @@ for i in range(1, 101):
     else:
         print(i)
 #3
+def najmniejsza_wartosc(lista):
+    if not lista:
+        return None
+    najmniejsza = lista[0]
+    for liczba in lista:
+        if liczba < najmniejsza:
+            najmniejsza = liczba
+    return najmniejsza
+
+#4
+def odwroc_ciag(ciag):
+    odwrocony = ""
+    for i in range(len(ciag) - 1, -1, -1):
+        odwrocony += ciag[i]
+    return odwrocony
+
+#1 rekurencja
+import sys
+
+def licz_rosnaco(n):
+    if n < 1:
+        return
+    
+    if n == 1:
+        print(1)
+    else:
+        licz_rosnaco(n - 1)
+        print(n)
+
+try:
+    liczba = int(input("Podaj liczbę: "))
+    licz_rosnaco(liczba)
+except RecursionError:
+    print("i'm actually not sure about that (⁠๑⁠•⁠﹏⁠•⁠) - liczba jest za duża dla rekurencji!")
+except Exception as e:
+    print(f"Wystąpił błąd....")
+
+def wypisz_rosnaco(n):
+    if n < 1:
+        return
+    wypisz_rosnaco(n - 1)
+    print(n)
+
+#2 rekurencja
+def silnia_rekurencyjna(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * silnia_rekurencyjna(n - 1)
