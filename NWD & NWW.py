5665dd5d672d0b9b65199d7dccbec5b40a934ffe
jan4wlaz-hube <jan4wlaz@gmail.com>
2026-06-10 11:43:06 +0200

    xx

---
+++ NWD & NWW.py
@@ -0,0 +1,22 @@
+#27VI2026
+def nwd_odejmowanie(a,b):
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+def nwd_modulo(a,b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def nww(a,b):
+    return (a * b) // nwd_modulo(a,b)
+
+liczba1 = int(input("Podaj pierwszą liczbę: "))
+liczba2 = int(input("Podaj drugą liczbę: "))
+print(nwd_odejmowanie(liczba1, liczba2))
+print(nwd_modulo(liczba1, liczba2))
+print(nww(liczba1, liczba2))
