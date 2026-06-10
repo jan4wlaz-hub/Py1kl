02aaf57b78bc8d44299763994879ab2bb9612334
jan4wlaz-hube <jan4wlaz@gmail.com>
2026-06-10 10:54:13 +0200

    commit 1234567890abcdef1234567890abcdef12345678 Author: Jan

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    while True:
+        print("\nWybierz program do uruchomienia:")
+        print("1. Program 1")
+        print("2. Program 2")
+        print("3. Program 3")
+        print("4. Wyjdź")
+        choice = input("Podaj numer programu: ")    
+        
