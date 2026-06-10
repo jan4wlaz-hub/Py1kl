e733b2dea7a3e4d271980fbb4e7fd9395e931e68
jan4wlaz-hube <jan4wlaz@gmail.com>
2026-06-10 12:10:08 +0200

    12:10

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 def main():
     while True:
         print("\nWybierz program do uruchomienia:")
-        print("1. Program 1")
+        print("1. NWD, NWW, ")
         print("2. Program 2")
         print("3. Program 3")
         print("4. Wyjdź")
         choice = input("Podaj numer programu: ")    
-        
+
+#tasks:
+#Szukanie najmniejszego lub największego elementu w liście, 
+#Odwracanie kolejności liter w podanym wyrazie, 
+#Zliczanie wystąpień podanego znaku w tekście, 
